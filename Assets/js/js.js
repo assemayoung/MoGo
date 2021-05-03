d2d5599d2b639d9e6ff0978828e8500617b6a393
Assem <koffka.rd@gmail.com>
2021-05-04 01:19:29 +0600

    MoGo
    MoGo

---
+++ Assets/js/js.js
@@ -0,0 +1,121 @@
+ $(document).ready(function(){
+ 		 $("#btn_tx2").hide();
+ 		 $("#btn_tx3").hide();
+	$('.slider').slick();
+	// $('.slider').slick({
+ //      prevArrow: $('#prev'),
+ //      nextArrow: $('#aft'),
+ //  });
+ 	// $('#btn1').on('click',function(){
+ 	// 	$('#btn_tx1').slideToggle(1000);
+ 	// })
+ 	// $('#btn2').on('click',function(){
+ 	// 	$('#btn_tx2').slideToggle(1000);
+ 	// })
+ 	// $('#btn3').on('click',function(){
+ 	// 	$('#btn_tx3').slideToggle(1000);
+ // 	// })
+ // })
+ // $(document).ready(function(){
+ 	$("#btn1").click(function(){
+ 		$("#btn_tx1").slideToggle("togled");
+ 	})
+ 	$("#btn2").click(function(){
+ 		 $("#btn_tx2").slideToggle("togled");
+ 	})
+ 	$("#btn3").click(function(){
+ 		$("#btn_tx3").slideToggle("togled");
+ 	})
+})
+
+//  $(document).ready(function () {
+//     $('.your-class').slick({
+//       prevArrow: $('#slide_l'),
+//       nextArrow: $('#slider_r'),
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//  	$('#btn_width').on('click',function(){
+//  		$('.block').animate({'width':'200px'},1000);
+//  	})
+//  	$('#btn_opacity').on('click',function(){
+//  		$('.block').animate({'opacity':'0'},1000);
+//  	})
+//  	$('#btn_hide').on('click',function(){
+//  		$('.block').hide(1000);
+//  	})
+//  	$('#btn_show').on('click',function(){
+//  		$('.block').show(1000);
+//  	})
+//  	$('#btn_toggle').on('click',function(){
+//  		$('.block').hide(1000).delay(1000).show(1000);
+//  	})
+//  	$('#btn_fadeout').on('click',function(){
+//  		$('.block').fadeOut(1000);
+//  	})
+//  	// $('#btn_fadein').on('click',function(){
+//  	// 	$('.block').fadeIn(1000);
+//  	// })
+//  	$('#btn_inout').on('click',function(){
+//  		$('.block').fadeToggle(1000);
+//  	})
+//  	$('#btn_slide-hide').on('click',function(){
+//  		$('.block').slideUp(1000);
+//  	})
+//  	$('#btn_slide-show').on('click',function(){
+//  		$('.block').slideDown(1000);
+//  	})
+//  	$('#btn_show-hide').on('click',function(){
+//  		$('.block').slideToggle(1000);
+//  	})
+//  })
+
---
+++ Assets/js/main.js
@@ -0,0 +1,17 @@
+// (function($) {
+    
+//   var allPanels = $('.test3_txt > dd').hide();
+    
+//   $('.test3_txt > dt > a').click(function() {
+//       $this = $(this);
+//       $target =  $this.parent().next();
+
+//       if(!$target.hasClass('active')){
+//          allPanels.removeClass('active').slideUp();
+//          $target.addClass('active').slideDown();
+//       }
+      
+//     return false;
+//   });
+
+// })(jQuery);
